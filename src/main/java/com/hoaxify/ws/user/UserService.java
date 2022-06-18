39d56c54225ac351fe93a6b138dc66c5cbc2e1ec
= <=>
2022-06-19 01:15:04 +0300

    EncodedPasswordWithSpringSecurity

--- src/main/java/com/hoaxify/ws/user/UserService.java
+++ src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,5 +1,7 @@
 package com.hoaxify.ws.user;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,13 +10,17 @@ public class UserService {
 	
 	UserRepository userRepository;
 	
+	PasswordEncoder passwordEncoder;
+	
 	//bir classta sadece bi constructor varsa Autowirde kullanmamız çok gerekli değil
 	public UserService(UserRepository userRepository) {
 		super();
 		this.userRepository = userRepository;
+		this.passwordEncoder = new BCryptPasswordEncoder();
 	}
 	
 	public void save(User user) {
+		user.setPassword(this.passwordEncoder.encode(user.getPassword()));
 		userRepository.save(user);
 	}
 	
