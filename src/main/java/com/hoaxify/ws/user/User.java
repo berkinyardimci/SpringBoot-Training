f4e795f84b8cf05053c7b7089030f7009ed5c96a
= <=>
2022-06-18 03:51:11 +0300

    ConnectH2Database

---
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -0,0 +1,24 @@
+package com.hoaxify.ws.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String username;
+	private String displayName;
+	private String password;
+	
+	
+}
