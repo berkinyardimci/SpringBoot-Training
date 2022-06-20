f16b238b531f5a65803dd66669f44bc7bad45ed6
= <=>
2022-06-20 04:04:53 +0300

    CreateValidationErrors

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -16,8 +18,16 @@ public class User {
 	@GeneratedValue
 	private long id;
 	
+	@NotNull
+	@Size(min=4,max=255)
 	private String username;
+	
+	@NotNull
 	private String displayName;
+	
+	@NotNull
+	@Size(min=4,max=255)
+	//@Pattern(regexp = "^(?=.*[a-z])[A-Z].$")
 	private String password;
 	
 	
